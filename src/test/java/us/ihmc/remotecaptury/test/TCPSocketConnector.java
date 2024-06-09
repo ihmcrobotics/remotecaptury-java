ce9509cc8d99ce59c06449b25f25786fc5934347
Ben <bpratt@ihmc.org>
2024-06-08 20:51:06 -0500

    Sets up TCPSocketConnection between computers so the recieving computer doesn't recieve as much data

---
+++ src/test/java/us/ihmc/remotecaptury/test/TCPSocketConnector.java
@@ -0,0 +1,40 @@
+package us.ihmc.remotecaptury.test;
+
+import java.net.*;
+import java.io.*;
+
+public class TCPSocketConnector
+{
+   private ServerSocket serverSocket;
+   private Socket clientSocket;
+   private PrintWriter out;
+   private BufferedReader in;
+
+   public void start(int port) throws IOException
+   {
+      serverSocket = new ServerSocket(port);
+      clientSocket = serverSocket.accept();
+      out = new PrintWriter(clientSocket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String greeting = in.readLine();
+      if("hello server".equals(greeting)){
+         out.println("hello client");
+      }
+      else{
+         out.println("unrecognised greeting");
+      }
+   }
+
+   public void stop() throws IOException
+   {
+      in.close();
+      out.close();
+      clientSocket.close();
+      serverSocket.close();
+   }
+   public static void main(String[] args) throws IOException
+   {
+      TCPSocketConnector server = new TCPSocketConnector();
+      server.start(6666);
+   }
+}
---
+++ src/test/java/us/ihmc/remotecaptury/test/TestTCPSocketConnection.java
@@ -0,0 +1,19 @@
+package us.ihmc.remotecaptury.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTCPSocketConnection
+{
+   @Test
+   public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException
+   {
+      TCPSocketClient client = new TCPSocketClient();
+      client.startConnection("127.0.0.1", 6666);
+      String response = client.sendMessage("hello server");
+      assertEquals("hello client", response);
+   }
+}
